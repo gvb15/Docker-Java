70961f44f507e02634caae0de816c8ce3551b5a5
gvb15 <148179632+gvb15@users.noreply.github.com>
2026-06-27 20:43:59 +0530

    Create WelcomeToDocker.java

---
+++ WelcomeToDocker.java
@@ -0,0 +1,27 @@
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class WelcomeToDocker {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/", (HttpExchange exchange) -> {
+            String response = "Welcome to Docker!";
+
+            exchange.sendResponseHeaders(200, response.length());
+
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
+        server.start();
+
+        System.out.println("Server started on port 8080");
+    }
+}
